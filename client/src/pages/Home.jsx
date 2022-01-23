4a9d3d6e2ecddf3cb0c42a97de3bb9df9080852e
raniabrn <raniabrrn@gmail.com>
2022-01-23 22:15:43 +0000

    site

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+import Products from "../components/Products";
+import Slider from "../components/Slider";
+
+const Home = () => {
+  return (
+    <div>
+     <Navbar />
+      <Slider />
+      <Products/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { login } from "../redux/apiCalls";
+import { mobile } from "../responsive";
+import { useDispatch, useSelector } from "react-redux";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color : rgb(224,205,169);
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+  border-radius: 17px;
+  ${mobile({ width: "75%" })}
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+  position: relative;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  
+  padding: 10px;
+`;
+
+const Button = styled.button`
+display: block;
+border: none;
+outline: none;
+font-size: 1.7rem;
+font-weight: 510;
+text-transform: uppercase;
+letter-spacing: 1px;
+border-radius: 5px;
+cursor: pointer;
+color: white;
+border-radius: 8px;
+background-color: black;
+position: relative;
+width: 100%;
+height: 60px;
+left: -1%;
+`;
+
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const Error = styled.span`
+  color: red;
+`;
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, password });
+  };
+  return (
+    <Container>
+      <Wrapper>
+        <Title><b>SIGN IN</b></Title>
+        <Form>
+          <Input
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            placeholder="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button onClick={handleClick} disabled={isFetching}>
+            LOGIN
+          </Button>
+          {/* {error && <Error>Something went wrong...</Error>} */}
+          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
